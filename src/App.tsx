b9208540aa5a3e72f0cb8c1c3d74fdaa46d3f14a
Yakkiiz <yakkathehutt@gmail.com>
2024-09-11 00:16:42 +0700

    Create Layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex-col flex items-center border-8 border-emerald-800 font-saira font-semibold">
+        <div className="w-full text-center py-5 mt-2 border-b-8 border-emerald-800 text-7xl">Portfolio</div>
+        <div className="w-full my-5 ms-10">
+          <div className="mb-6">
+            <div>Phone</div>
+            <div>E-Mail</div>
+          </div>
+          <div>
+            <div className="text-4xl">Languages</div>
+            <div className="text-xl">English</div>
+            <div className="text-xl">Thai</div>
+          </div>
+          <div>
+            <div className="text-4xl">Liking</div>
+            <div className="text-2xl">Movies</div>
+            <div className="text-2xl">Coding</div>
+            <div className="text-2xl">Gaming</div>
+          </div>
+          <div>aboutMe</div>
+          <div>education</div>
+          <div>experience</div>
+          <div>skill</div>
+          <div>workedAs</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
