28a23710d4c4747db02de5c5597770fca485bbb3
Yakkiiz <yakkathehutt@gmail.com>
2024-09-12 23:38:55 +0700

    add icon in about ,edit text in about & chang text color all page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center border-8 border-emerald-800 font-saira font-semibold mx-8">
-        <div className="mt-2 w-full border-b-8 border-emerald-800 py-5 text-center text-7xl">
+      <div className="mx-8 flex flex-col items-center border-8 border-emerald-800 font-saira font-semibold">
+        <div className="mt-2 w-full border-b-8 border-emerald-800 py-5 text-center text-7xl text-emerald-600">
           Portfolio
         </div>
-        <div className="flex w-full flex-col gap-y-7 py-5 pl-5">
+        <div className="flex w-full flex-col gap-y-7 py-5 pl-5 text-emerald-600">
           <div className="">
             <div>Phone</div>
             <div>E-Mail</div>
@@ -23,15 +23,29 @@ function App() {
             <div className="text-2xl">Coding</div>
             <div className="text-2xl">Gaming</div>
           </div>
-          <div className="text-4xl absolute right-2 border-emerald-600 border-4 bg-white rounded-lg w-96 text-center">About Me
-            <div className="border-t-4 border-emerald-600 bg-white text-left">
-              <div>Apiwat</div>
-              <div>Songnilrak</div>
+          <div className="absolute right-2 w-2/6 rounded-lg border-4 border-emerald-600 bg-white text-center text-4xl">
+            About Me
+            <div className="border-t-4 border-emerald-600 bg-emerald-500 text-left text-white">
+              <div className="ml-3 mt-4">Apiwat</div>
+              <div className="ml-3 mb-1">Songnilrak</div>
               <div>
-                <div className="text-2xl">Tel</div>
-                <div className="text-2xl">Email</div>
-                <div className="text-2xl">Address</div>
-                <div className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex harum dicta sapiente aliquid, facere laborum accusamus eaque provident fugit, inventore aut modi eius at? Sit minus consectetur impedit officiis perferendis?</div>
+                <div className="text-2xl ml-3">
+                  <i className="uil uil-phone">(+66)85-363-3779</i>
+                </div>
+                <div className="text-2xl ml-3">
+                  <i className="uil uil-envelope-alt">arm_apiwat@outlook.com</i>
+                </div>
+                <div className="text-2xl ml-3">
+                  <i className="uil uil-location-point mb-3">
+                    44/9 M.4 Bangyor Samutprakarn 10130
+                  </i>
+                </div>
+                <div className="text-base ml-3">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
+                  harum dicta sapiente aliquid, facere laborum accusamus eaque
+                  provident fugit, inventore aut modi eius at? Sit minus
+                  consectetur impedit officiis perferendis?
+                </div>
               </div>
             </div>
           </div>
