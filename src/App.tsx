9c7f3e84ccb3b09fba86a8742ea086e39f3588c0
Yakkiiz <yakkathehutt@gmail.com>
2024-09-14 00:43:30 +0700

    add image and fix border radius padding & edit text in about

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import profileImage from "/ProfileProject.png";
 
 function App() {
   return (
     <>
-      <div className="mx-8 flex flex-col items-center border-8 border-emerald-800 font-saira font-semibold">
+      <div className="flex flex-col items-center border-8 border-emerald-800 font-saira font-semibold">
         <div className="mt-2 w-full border-b-8 border-emerald-800 py-5 text-center text-7xl text-emerald-600">
           Portfolio
         </div>
@@ -23,24 +24,24 @@ function App() {
             <div className="text-2xl">Coding</div>
             <div className="text-2xl">Gaming</div>
           </div>
-          <div className="absolute right-2 w-2/6 rounded-lg border-4 border-emerald-600 bg-white text-center text-4xl">
+          <div className="absolute right-2 w-2/6 overflow-hidden rounded-3xl border-4 border-emerald-600 bg-white text-center text-4xl">
             About Me
-            <div className="border-t-4 border-emerald-600 bg-emerald-500 text-left text-white">
-              <div className="ml-3 mt-4">Apiwat</div>
-              <div className="ml-3 mb-1">Songnilrak</div>
+            <div className="border-t-4 border-emerald-600 bg-emerald-500 pb-5 pl-3 text-left text-white">
+              <div className="mt-4">Apiwat</div>
+              <div className="mb-1">Songnilrak</div>
               <div>
-                <div className="text-2xl ml-3">
-                  <i className="uil uil-phone">(+66)85-363-3779</i>
+                <div className="text-2xl">
+                  <i className="uil uil-phone"></i> (+66)85-363-3779
                 </div>
-                <div className="text-2xl ml-3">
-                  <i className="uil uil-envelope-alt">arm_apiwat@outlook.com</i>
+                <div className="text-2xl">
+                  <i className="uil uil-envelope-alt"></i>{" "}
+                  arm_apiwat@outlook.com
                 </div>
-                <div className="text-2xl ml-3">
-                  <i className="uil uil-location-point mb-3">
-                    44/9 M.4 Bangyor Samutprakarn 10130
-                  </i>
+                <div className="text-2xl">
+                  <i className="uil uil-location-point mb-3"></i> 44/9 M.4
+                  Bangyor Samutprakarn 10130
                 </div>
-                <div className="text-base ml-3">
+                <div className="text-base">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
                   harum dicta sapiente aliquid, facere laborum accusamus eaque
                   provident fugit, inventore aut modi eius at? Sit minus
@@ -48,6 +49,9 @@ function App() {
                 </div>
               </div>
             </div>
+          </div>
+          <div className="absolute h-[250px] w-[250px] inset-x-2/4">
+            <img src={profileImage} alt="" />
           </div>
           <div className="flex flex-col gap-y-7">
             <div className="text-4xl">Education</div>
